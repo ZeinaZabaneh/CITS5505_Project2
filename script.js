890092807d9ac9b52d0fcf66e3e533c324ed594a
Ben Willcocks <ben@bens-mbp.lan>
2022-05-17 17:59:01 +0800

    hint box function partialy complete

--- script.js
+++ script.js
@@ -19,18 +19,54 @@ document.querySelector("#close").addEventListener("click", function(){
 
 // Game Functionality
 
+var code_to_guess = [1,2,3,4];
+
+var correct_guess = false;
+
 var active_row = 0;
 
 function row_change() {
 
-	$('.row-'+active_row).css('pointer-events', 'none');
+	active_row += 1;
+
+	if(active_row>1){
+
+		var comparison_guess = code_to_guess;
+		var user_guess = [];
+
+		const boxes_in_previous_row = document.getElementsByClassName("row-"+(active_row-1));
+
+		for(let i=0; i<code_to_guess.length; i++) {
+			var guess = boxes_in_previous_row[i].getAttribute("data-colour_id");
+			user_guess.push(guess);
+		}
+
+		//var out_of_place_count = 0;
+		var correct_place_count = 0;
+
+		for(let i=0; i<code_to_guess.length; i++) {
+
+			var guess = user_guess[i]
+			var actual = code_to_guess[i];
+
+			if(guess==actual){
+				correct_place_count += 1;
+				user_guess.splice(i, 1);
+				comparison_guess.splice(i, 1);
+				i -= 1;
+			}
+		}
+
+		$('.correct_place_row_'+(active_row-1)).text(correct_place_count);
+	}
+
+
+	$('.row-'+(active_row-1)).css('pointer-events', 'none');
 
 	$('#enter_button').css('pointer-events', 'none');
 	$('#enter_button').css('opacity', '40%');
 	$('#enter_button').css('animation', 'none');
 	$('#enter_button').text('Enter Guess');
-	
-	active_row += 1;
 
 	$('.row-'+active_row).click(function(event) {
 
