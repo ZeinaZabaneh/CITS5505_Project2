a452c2aba31a20e276c9ed8532ccbd968240c76e
Ben Willcocks <ben@bens-mbp.lan>
2022-05-17 16:19:04 +0800

    check guess and enter button working, hint box functionality not present

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ var active_row = 0;
 function row_change() {
 
 	$('.row-'+active_row).css('pointer-events', 'none');
+
+	$('#enter_button').css('pointer-events', 'none');
+	$('#enter_button').css('opacity', '40%');
+	$('#enter_button').css('animation', 'none');
+	$('#enter_button').text('Enter Guess');
 	
 	active_row += 1;
 
@@ -36,21 +41,39 @@ function row_change() {
 		$(box_id).attr('data-colour_id', ((parseInt($(box_id).attr('data-colour_id'))+1)%5))
 	  
 		$(box_id).css('background-color', colours[parseInt($(box_id).attr('data-colour_id'))]);
-	
-	});
 
-	if(active_row>1){
+		// console.log($(box_id).attr('data-colour_id'));
+
+		//var box_element = document.getElementById($(this).attr('id'));
+
+		//console.log(box_element.getAttribute("data-colour_id"));
+		
+		//box_element.setAttribute('data-colour_id', $(box_id).attr('data-colour_id'));
+
+		//console.log(box_element.getAttribute("data-colour_id"));
 
 		const boxes_in_active_row = document.getElementsByClassName("row-"+active_row);
+
+		var valid_row_input = true;
 
 		for(let box of boxes_in_active_row){
 
 			var check = box.getAttribute("data-colour_id");
-			console.log(check);
+			if(check=='19'){
+				valid_row_input = false;
+			}
 
 		}
 
-	}
+		if(valid_row_input){
+			$('#enter_button').css('pointer-events', 'auto');
+			$('#enter_button').css('opacity', '100%');
+			$('#enter_button').text('Check!');
+		}
+	
+	
+	});
+
 
 }
 
